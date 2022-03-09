eac2c5cb382102a7a84a673a090037b0037c6855
xlemx <magdalena.lem011@o2.pl>
2022-03-09 20:07:17 +0100

    Ad.7gg

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -2,7 +2,10 @@ package pl.lublin.wsei.java.cwiczenia;
 
 public class Main {
     public static void main(String[] args) {
-      System.out.print("Nazwaliśmy to \"witaminą B3\"");
-
+        System.out.printf("alfa\tsin(alfa)\n");
+        int i;
+        for (i = 0; i < 370; i += 10) {
+        System.out.printf("%d\t%f\t\n", i, Math.sin(i / 360.02 * Math.PI));
+        }
     }
 }
