5b2b8ae63285309e02ca2a719c7170a32ecb809b
xlemx <magdalena.lem011@o2.pl>
2022-03-09 19:26:12 +0100

    Ad.7e

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -1,14 +1,14 @@
 package pl.lublin.wsei.java.cwiczenia;
 
 public class Main {
-    public static void /** komentarz Javadoc */main(String[] args) {
-        //komentarz jednolinijkowy
-        /*komentarz
-        * wielolinijkowy*/
+    public static void main(String[] args) {
+       int a=3;
+       double b= 4.21;
+       String s= "jakiś tekst";
 
-        System.out.println("Ala\n");
-        System.out.println("man\n");
-        System.out.println("kota\n");
+       System.out.printf("a= %d, b=%f, s=%s %n",a,b,s);
+       System.out.printf("a= %d, b=%.2f, s=%20s %n",a,b,s);
+
 
 
 
