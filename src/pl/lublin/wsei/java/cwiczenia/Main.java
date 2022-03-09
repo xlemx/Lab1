9a5c84deeb64eac096c9dce4b0800eaa4ed3e0f6
xlemx <magdalena.lem011@o2.pl>
2022-03-09 19:39:05 +0100

    Ad.7g

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -2,15 +2,7 @@ package pl.lublin.wsei.java.cwiczenia;
 
 public class Main {
     public static void main(String[] args) {
-       int a=3;
-       double b= 4.21;
-       String s= "jakiś tekst";
-
-       System.out.printf("a= %d, b=%f, s=%s %n",a,b,s);
-       System.out.printf("a= %d, b=%.2f, s=%20s %n",a,b,s);
-
-
-
+      System.out.print("Nazwaliśmy to \"witaminą B3\"");
 
     }
 }
