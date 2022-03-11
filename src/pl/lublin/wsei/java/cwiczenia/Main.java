8c6083b3fc25f995f565434fa9684f3bf3d51de8
xlemx <magdalena.lem011@o2.pl>
2022-03-11 11:14:12 +0100

    Ad.7h

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -1,11 +1,15 @@
 package pl.lublin.wsei.java.cwiczenia;
-
 public class Main {
+
     public static void main(String[] args) {
-        System.out.printf("alfa\tsin(alfa)\n");
-        int i;
-        for (i = 0; i < 370; i += 10) {
-        System.out.printf("%d\t%f\t\n", i, Math.sin(i / 360.02 * Math.PI));
+        System.out.print("arg\t\t\tlog2(arg)\t\t\tsum(arg)\n");
+        double LOG2 = Math.log(2);
+        int i = 1;
+        while (i < 10) {
+            double x = i;
+            i = i * 2;
+
+            System.out.printf("%f\t%f\t\n", x, Math.log(x) / LOG2);
         }
     }
 }
