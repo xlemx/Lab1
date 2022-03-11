067454aceb41010394341f94c0ebe00d4265066b
xlemx <magdalena.lem011@o2.pl>
2022-03-11 15:24:12 +0100

    Ad.7.d.

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -1,15 +1,21 @@
 package pl.lublin.wsei.java.cwiczenia;
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.print("arg\t\t\tlog2(arg)\t\t\tsum(arg)\n");
-        double LOG2 = Math.log(2);
-        int i = 1;
-        while (i < 10) {
-            double x = i;
-            i = i * 2;
+        Scanner input = new Scanner(System.in);
+        int num1=0, num2=0;
+        do {
 
-            System.out.printf("%f\t%f\t\n", x, Math.log(x) / LOG2);
-        }
+            System.out.print("Podaj pierwszą liczbę: ");
+            num1 = input.nextInt();
+            if (num1 == 0) break ;
+            System.out.print("Podaj drugą liczbę: ");
+            num2 = input.nextInt() ;
+            if (num2==0) break;
+            System.out.printf("Wynik dodawania %d + %d = %d%n", num1, num2, num1 + num2);
+        } while (true);
     }
 }
