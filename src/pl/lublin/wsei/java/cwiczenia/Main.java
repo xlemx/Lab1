3d57eee1e78fd8a1d3c110cce6768b7ee479d3ac
xlemx <magdalena.lem011@o2.pl>
2022-03-14 00:37:11 +0100

    Ad.12

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -1,24 +1,13 @@
 package pl.lublin.wsei.java.cwiczenia;
+import pl.lublin.wsei.java.cwiczenia.mylib.Account;
+
 import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        int[] liczby = new int[30];
-        Random rnd = new Random();
-
-        for (int i = 0; i < 30; i++)
-            liczby[i] = rnd.nextInt();
-
-        int mx = Integer.MIN_VALUE;
-        int mn = Integer.MAX_VALUE;
-        long avg = 0;
-        for (int l : liczby) {
-            System.out.println(l);
-            if (l < mn) mn = l;
-            if (l > mx) mx = l;
-            avg += l;
-        }
-        System.out.printf("MIN = %d, MAX = %d, AVG = %f",mn,mx,(float)avg/liczby.length);
+        Account acc = new Account();
+        acc.setName("Piotr Gołabek");
+        System.out.println(acc.getName());
     }
 }
 
---
+++ src/pl/lublin/wsei/java/cwiczenia/mylib/Account.java
@@ -0,0 +1,12 @@
+package pl.lublin.wsei.java.cwiczenia.mylib;
+
+public class Account {
+    private String name;
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+
+}
