f3ded6830917bf118506fed68c0ccc1de73fde68
xlemx <magdalena.lem011@o2.pl>
2022-03-14 12:53:36 +0100

    Ad.12.g

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -1,12 +1,11 @@
 package pl.lublin.wsei.java.cwiczenia;
 import pl.lublin.wsei.java.cwiczenia.mylib.Account;
 
-import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
         Account acc = new Account();
-        acc.setName("Piotr Gołabek");
+        acc.setName("piotr Gołabek");
         System.out.println(acc.getName());
     }
 }
--- src/pl/lublin/wsei/java/cwiczenia/mylib/Account.java
+++ src/pl/lublin/wsei/java/cwiczenia/mylib/Account.java
@@ -1,9 +1,13 @@
 package pl.lublin.wsei.java.cwiczenia.mylib;
-
+import org.apache.commons.lang3.StringUtils;
 public class Account {
     private String name;
+
     public void setName(String name) {
-        this.name = name;
+        String[] tokens = StringUtils.split(name);
+        for(int i = 0; i < tokens.length; i++)
+            tokens[i] = StringUtils.capitalize(tokens[i]);
+        this.name = StringUtils.join(tokens,' ');
     }
     public String getName() {
         return name;
