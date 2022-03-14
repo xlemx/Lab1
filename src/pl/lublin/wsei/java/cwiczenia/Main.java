dfe175eeaf30d914716a49caf9f08f9aeba8b2f8
xlemx <magdalena.lem011@o2.pl>
2022-03-14 13:01:49 +0100

    Ad.12.h

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -1,12 +1,12 @@
 package pl.lublin.wsei.java.cwiczenia;
 import pl.lublin.wsei.java.cwiczenia.mylib.Account;
 
-
 public class Main {
     public static void main(String[] args) {
         Account acc = new Account();
         acc.setName("piotr Gołabek");
-        System.out.println(acc.getName());
+        System.out.printf("%s %n",Account.capitalize("stanisŁaw maruSARz"));
+
     }
 }
 
--- src/pl/lublin/wsei/java/cwiczenia/mylib/Account.java
+++ src/pl/lublin/wsei/java/cwiczenia/mylib/Account.java
@@ -3,14 +3,15 @@ import org.apache.commons.lang3.StringUtils;
 public class Account {
     private String name;
 
-    public void setName(String name) {
-        String[] tokens = StringUtils.split(name);
+    public static String capitalize(String aStr)
+    {
+        String[] tokens = StringUtils.split(aStr);
         for(int i = 0; i < tokens.length; i++)
             tokens[i] = StringUtils.capitalize(tokens[i]);
-        this.name = StringUtils.join(tokens,' ');
+        return StringUtils.join(tokens,' ');
     }
-    public String getName() {
-        return name;
+    public void setName(String name) {
+        this.name = capitalize(name);
     }
 
 }
